e51af9bbd349c9ce53533ac552a67f11d5e93161
Poundednote <rayyan.naz.02@gmail.com>
2025-02-20 19:06:16 +0000

    update build file for java version

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 IF "%1" == "C" (
         javac -h ./ SharedMemory.java
-        cl /Febuild\ /Fobuild\ /Fdbuild\ /I"C:\Program Files\jdk-23.0.2\include" /I"C:\Program Files\jdk-23.0.2\include\win32" /LD shared_map.c Kernel32.lib
+        cl /Febuild\ /Fobuild\ /Fdbuild\ /I"C:\Program Files\jdk-21.0.6\include" /I"C:\Program Files\jdk-21.0.6\include\win32" /LD shared_map.c Kernel32.lib
         copy /Y build\shared_map.dll lib\shared_map.dll
 )
 
