e5dfdaf2697e90fc8fef04d8cb62d5cec54f6a31
Poundednote <rayyan.naz.02@gmail.com>
2025-02-20 14:51:15 +0000

    adding process files

--- Scheduler.java
+++ Scheduler.java
@@ -1,27 +1,38 @@
+import java.io.File;
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.LongBuffer;
+import java.nio.file.FileSystems;
+
+import static java.lang.System.getProperty;
 
 public class Scheduler {
     private static Scheduler singleton_ref = null;
     public static final int MAX_BLOCKS = 256*1024;
 
     //TODO(Ray) maybe change to dynamic arrays
-    private int[] block_pids;
-    private int[] block_times;
-    private String[] process_filenames;
+    public String projectName;
+    public String projectDir;
+    private int[] blockPids;
+    private int[] blockTimes;
+    private String[] processFilenames;
 
-    private int numProcesses = 0;
+    // zero is always reserved for invalid processes
+    private int numProcesses = 1;
 
-    private LongBuffer mutex_buffer;
-    private ByteBuffer signal_buffer;
+    private LongBuffer mutexBuffer;
+    private ByteBuffer signalBuffer;
 
     private Scheduler() {
-        this.mutex_buffer = SharedMemory.GetSharedMutexBuffer(256);
-        this.signal_buffer = SharedMemory.GetSignalArray();
+        SharedMemory.AlocWeaveSharedBuffer();
+        // NOTE:(Ray) Never use more than 255 as the argument to this function
+        // maybe make allocWeaveSharedBuffer take in paramters
+        this.mutexBuffer = SharedMemory.GetSharedMutexBuffer(255);
+        this.signalBuffer = SharedMemory.GetSignalArray();
 
-        this.block_pids  = new int[MAX_BLOCKS];
-        this.block_times  = new int[MAX_BLOCKS];
-        this.process_filenames = new String[256];
+        this.blockPids  = new int[MAX_BLOCKS];
+        this.blockTimes  = new int[MAX_BLOCKS];
+        this.processFilenames = new String[256];
 
         //TODO(Ray) eventually serialise block pids and times from file
     }
@@ -39,27 +50,43 @@ public class Scheduler {
     public void runProcesses() {
         //TODO(Ray) implement scheduling algorithm
         //
-        // 0 is invalid process id and should be skipped all process blocks will be time orderd and 
+        // 0 is invalid process id and should be skipped all process blocks will be time orderd and
         // tagged with their position along the time axis
         // [0, 0, 1, 2] would be 1 process that is invalid, and a second process with ID 1 at position 2 on the time axis
         //
         //
         //
         //
-        //  TIME INDEXED ARRAY WHERE EACH OF THE 1024 TIME SLOTS HAS 256 PROCESS SLOTS all vailid pids at index 0-256 
+        //  TIME INDEXED ARRAY WHERE EACH OF THE 1024 TIME SLOTS HAS 256 PROCESS SLOTS all vailid pids at index 0-256
         //  run first and then must wait for the next time block so on and so on
         //  MAYBE TREAT PROCESSES AS ENTITIES GIVE THEM UI_X AND UI_Y FOR RENDERING STORE THEIR IDS
         //
-        StringBuilder processString = new StringBuilder();
-        for (int i = 0; i < numProcesses; ++i) {
-
-        }
-        
     }
 
-    public int schedulerAddProcess(String filename) {
+    public int addProcess() {
         int pid = this.numProcesses++;  // get the PID
-        this.process_filenames[pid] = filename;
+        StringBuilder filename = new StringBuilder(this.projectName);
+        filename.append("_PROCESS_");
+        filename.append(pid);
+        filename.append(".py");
+        String fileSeperator = FileSystems.getDefault().getSeparator();
+
+        String fullFilepath = this.projectDir + fileSeperator + filename;
+
+        File file = new File(fullFilepath);
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            return 0;
+        }
+
         return pid;
+    }
+
+    public static void main(String args[]) {
+        Scheduler s = new Scheduler();
+        s.projectName = "TEST_PROJ";
+        s.projectDir = args[0];
+        s.addProcess();
     }
 }
