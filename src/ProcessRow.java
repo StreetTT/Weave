7108ceb6c459d85bd7bbbcd3dc5ed505aa7740a1
BridgeTheCode <contactjoshbr@gmail.com>
2025-04-10 19:35:11 +0100

    Tweaked row Ui again

--- src/ProcessRow.java
+++ src/ProcessRow.java
@@ -10,13 +10,14 @@ import javax.swing.*;
 public class ProcessRow extends HBox {
 
     public JFXButton deleteButton;
+    public JFXButton runButton;
 
     //TODO: make the row dynamically change size when resized
+
     public ProcessRow(WeaveProcess process) {
         //styling for row
         this.setPadding(new Insets(10));
         this.setAlignment(Pos.CENTER_LEFT);
-        this.setSpacing(50);
 
         //coinatiner for a rounded rectangle look
         VBox roundedContainer = new VBox();
@@ -30,12 +31,16 @@ public class ProcessRow extends HBox {
                         "-fx-border-width: 1px;"
         );
         roundedContainer.setMaxWidth(Double.MAX_VALUE);
+        HBox.setHgrow(roundedContainer,Priority.ALWAYS);
 
 
-        //Top bar with delete and (TODO: MAkE Run button)
+
+
+        //Top bar with delete and run button
         AnchorPane topBar = new AnchorPane();
         topBar.setPrefHeight(30);
 
+        //TODO: Find a nice icon for button using a lib
         this.deleteButton = new JFXButton("X");
         deleteButton.setRotate(90);
         deleteButton.setStyle(
@@ -48,9 +53,25 @@ public class ProcessRow extends HBox {
         deleteButton.setTranslateY(-15);
         AnchorPane.setLeftAnchor(deleteButton, 10.0);
 
-        topBar.getChildren().add(deleteButton);
+        //TODO: Functionaility for this buttton lol
+        this.runButton = new JFXButton("▶");
+        runButton.setRotate(90);
+        runButton.setStyle(
+                "-fx-background-color: #81c784;" +
+                        "-fx-text-fill: white;" +
+                        "-fx-background-radius: 0 15 15 0;" +
+                        "-fx-padding: 4 10 4 10;"
+        );
 
-        //showing name of process (TODO: link name to procces)
+        runButton.setTranslateY(-15);
+        AnchorPane.setLeftAnchor(runButton, 50.0);
+
+
+        topBar.getChildren().addAll(deleteButton, runButton);
+
+        // CONTENT BOX AREA
+
+        //showing name of proces(TODO: link name to procces)
         Label processName = new Label("EXAMPLE PROCESS NAME");
         processName.setStyle(
                 "-fx-background-color: #eeeeee;" +
@@ -58,17 +79,23 @@ public class ProcessRow extends HBox {
                         "-fx-font-size: 14px;" +
                         "-fx-background-radius: 5;"
         );
-        processName.setPrefWidth(200);
+
+        processName.setMinWidth(200);
 
         //row of processes inside the rectangle
         GridPaneRow newRow = new GridPaneRow(process);
 
         HBox blockWrapper = new HBox(newRow);
         blockWrapper.setAlignment(Pos.CENTER_LEFT);
-        blockWrapper.setPadding(new Insets(0, 0, 0, 0 ));
 
+        HBox contentBox = new HBox();
+        contentBox.setSpacing(20);
+        contentBox.setAlignment(Pos.CENTER_LEFT);
+        contentBox.getChildren().addAll(processName, blockWrapper);
+
+        VBox.setVgrow(contentBox, Priority.ALWAYS);
         //put all of the objects together
-        roundedContainer.getChildren().addAll(topBar, processName, blockWrapper);
+        roundedContainer.getChildren().addAll(topBar, contentBox);
         this.getChildren().add(roundedContainer);
     }
 }
